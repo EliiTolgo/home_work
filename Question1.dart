67fe241a4792223481342eb209d37a8566d7ccaf
Nada Nassar <nadagamal1699@gmail.com>
2025-02-13 19:45:53 +0200

    Add files via upload

---
+++ Question1.dart
@@ -0,0 +1,12 @@
+//Using Dot Operator with Strings
+//Objective: Practice using the dot operator with string methods.
+//Instructions:
+//- Create a string variable greeting with the value 'Hello, Dart!'.
+//- Print the length of greeting.
+//- Convert greeting to uppercase and print it.
+
+void main() {
+  String value = 'Hello, Dart!';
+  print(value.length);
+  print(value.toUpperCase());
+}
